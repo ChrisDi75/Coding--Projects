361572a74c72d5add339b7d9954370db5fad0746
ChrisDi75 <104084510+ChrisDi75@users.noreply.github.com>
2022-07-12 21:25:03 +0200

    excericse for loop

--- JavaScript-Crashcourse/app.js
+++ JavaScript-Crashcourse/app.js
@@ -1,13 +1,27 @@
-// let count = 1;
+// excercise: write a for loop that loops through 1 to 20 
+// if the number is divisible by 3, print frontend
+// if the number is divisible by 5, print simplified
+// if the number is divisible by 3 and 5, print frontend simplified
+// if the number is not divisible by either 3 or 5, print the number 
 
 
-// // while(count <= 100) {
-// //     console.log(count);
-// //     count = count + 1;
-// // }
+for (let i = 1; i <= 20; i++ ) {
+    if ( i % 3 === 0 ) {
 
+    
+    console.log(`${i} --> Frontend`);
+    }
 
-for ( let i = 1; i < 100; i++) {
-    console.log(i);
+    if ( i % 5 === 0 ) {
+        console.log(`${i} --> Simplified`);
+    }
 
-}
+    if ( i % 3 === 0 && i % 5 === 0 ) {
+        console.log(`${i} --> Frontend Simplified`);
+    }
+
+    if ( i % 3 !== 0 && i % 5 !== 0 ) {
+        console.log(i);
+    }
+    }
+
