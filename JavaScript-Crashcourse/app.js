a1c11f090142097e0bdfcbf6c6df2858048473ec
ChrisDi75 <104084510+ChrisDi75@users.noreply.github.com>
2022-07-13 02:05:54 +0200

    DOM intro

--- JavaScript-Crashcourse/app.js
+++ JavaScript-Crashcourse/app.js
@@ -115,5 +115,154 @@ console.log(`Welcome to FES, ${name}`);
 
 // console.log(newGrades);
 
+// same excercise as above but without filter method 
+// let grades= ["A+", "A", "B+", "FAIL"]; 
+// let goodGrades = [];
 
-let grades= ["A+", "A", "FAIL"]; 
+// for (let i = 0; i < grades.length; ++i) {
+//     if(grades[i] !== "FAIL") {
+//     goodGrades.push(grades[i]);
+
+//     }
+// }
+
+// console.log(goodGrades);
+
+
+// let arr = [1, 4, 9, 16];
+
+// let newArray = arr.map(element => "dog")
+
+// console.log(newArray);
+
+// EXCERCISE : Turn each element in an array of dollars into cents
+
+// let dollars = [1, 5, 10, 3];
+// let cents = [];
+
+
+// for(let i = 0; i < dollars.length; ++i) {
+//     cents.push(dollars[i] * 100);
+// console.log(cents);
+// }
+
+
+    
+
+
+
+
+
+
+
+
+
+//without using the map method 
+
+// let cents = dollars.map(element => element * 100 )
+
+// console.log(cents);
+
+
+// let dollarss= [0, 10, 20];
+
+// let centss = dollarss.map(element => element * 100);
+
+// console.log(centss);
+
+
+// let userFirstName = "David";
+// let unserLastName = "Bragg";
+// let userDiscordId = "David Brag#0001";
+// let userSubscriptionStatus = "VIP";
+
+// let users = [
+//     {
+//     username: "David", 
+//     email: "david@frontendsimplified.com",
+//     password: "test123",
+//     subscriptionStatus: "VIP",
+//     discordId: "David Brag#0001",
+//     lessonsCompleted:[0, 1]
+// }, 
+// {
+//     username: "Mitri", 
+//     email: "mitri@frontendsimplified.com",
+//     password: "mitri123",
+//     subscriptionStatus: "VIP",
+//     discordId: "Mitri #0001",
+//     lessonsCompleted:[0, 1, 2, 3],
+// }
+
+    // ];
+
+    // function login(email, password) {
+    //     for (i = 0; i < users.length; ++i){
+    //         if(users[i].email === email){
+    //             console.log(users[i]);
+    //             if(users[i].password === password){
+    //                 console.log("log the user in - the details are correct");
+    //             }
+    //             else{
+    //                 console.log("password is incorrect -try again")
+    //             }
+    //             return;
+    //         }
+    //     }
+    //     console.log("could not find an email that matches");
+    
+    // }
+
+    // login("david@frontendsimplified.com", "wrong Password");
+
+
+    // Create a register function that accepts:
+    // username, email, password, subscriptionStatus, discordId, lessonsCompleted
+    // Inside your register function: 1. create a user object, 2. push this user object onto the "users" array
+
+
+//     function register(user) {
+//     users.push(user);
+//  }
+
+
+
+//     register({
+//     username: "chris",
+//     email: "chris@frontendsimplified.com", 
+//     password : "testi123",
+//     subscriptionStatus: "VIP",
+//     discordId: "Chris #0001",
+//     lessonsCompleted: [0, 1, 2, 3, 4, 5, 6]
+// });
+
+//     // log in new user
+//     console.log(users);
+
+
+
+//DOM
+//First way of accessing an element
+// console.log(document.querySelector("h1"));
+
+
+//Second way of accessing an element
+
+// console.log(document.getElementById("title"));
+
+
+// Change String in Element 
+
+document.querySelector(".title").innerHTML ="Frontend Simplified";
+
+
+// Change CSS
+
+document.querySelector(".title").style.fontSize = "24px";
+
+function changeTitleToRed(){
+    document.querySelector(".title").style.color = "red"
+    console.log("clicked");
+}
+
+
