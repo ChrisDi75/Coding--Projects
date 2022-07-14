9d5e9c09b4810ef3365e8621eef17e2fc98f5e08
ChrisDi75 <104084510+ChrisDi75@users.noreply.github.com>
2022-07-14 22:29:03 +0200

    beginner challlenges

---
+++ JavaScript - Challenges/Beginner Challenges /app.js
@@ -0,0 +1,138 @@
+//  1. Return the Sum of 2 Numbers
+
+
+
+
+// function addition(num1, num2) {
+//     return num1 + num2;
+// }
+
+
+
+
+// console.log(addition(3, 5));
+
+
+
+
+// 2. Convert Hours into seconds 
+
+// function hours(num1) {
+//     return num1 * 3600;
+// }
+
+// console.log(hours(2));
+
+
+// 3. Calculate the Perimeter of a Rectangle 2* Länge + 2 * Breite
+
+// function calcPerimeter(length, width) {
+//     return 2 * length + 2 * width;
+// }
+
+// console.log(calcPerimeter(6, 7));
+
+// 4. Calculate the Area of a Triangle; Formula = Fläche 0.5 * base * height
+
+
+// function calcTriangleArea(base, height) {
+//     return 0.5 * base * height;
+// }
+
+// console.log(calcTriangleArea(10, 10));
+
+// 5. Extend a String - write a function that accepts a string and adds Frontend onto the end of it.
+
+
+// let name = "apple";
+
+// function appendFrontend(name) {
+//     return name + "Frontend";
+
+// }
+
+// console.log(appendFrontend(name));
+
+// 6. Greater than 100? Given two numbers, return true if the sum of both numbers is greater than 100. Otherweise, return false.
+
+// function sumGreaterThan100 (num1, num2) {
+//        return num1 + num2 > 100;
+
+
+// }
+
+// console.log(sumGreaterThan100(10, 20));
+
+// 7. Less than or Equal to Zero?
+
+// function lessThanOrEqualToZero (num1) {
+//     return num1 <= 0;
+// }
+
+// console.log(lessThanOrEqualToZero(3));
+
+//  8. Opposite boolean
+
+// function oppositeBoolean(bool) {
+//    return !bool;
+// }
+
+// console.log(oppositeBoolean(false));
+
+
+// 9. Is not the number 0 : Given any Element, return true if it is NOT the number 0
+
+// function isNotZero(element) {
+// return element != 0;
+// }
+// console.log(isNotZero(0));
+
+// 10. Calculate the remainder
+
+// function calcRemainder(num1, num2) {
+//     return num1 % num2;
+// }
+
+// console.log(calcRemainder(9, 8));
+
+// 11. Is the number odd? (ungerade)
+
+// function isOdd(num1) {
+// return num1 % 2 !== 0;
+//  }
+
+// console.log(isOdd(3));
+
+
+// 12. If a number is even, return 1 otherwise return -1 ( ternary Operator)
+
+
+
+// function booleanInteger(num){
+//     return num % 2 === 0 ? 1 : -1;
+// }
+
+// console.log(booleanInteger(2));
+
+
+//  13. Check if a user is logged in AND subscribed
+
+// function isLoggedInAndSubscribed(loggedIn, subscribed){
+//     return  loggedIn === "LOGGED_IN"  && subscribed === "SUBSCRIBED"  ;
+// }
+
+// console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"));
+
+// //  14. Check if a user is logged in OR subscribed
+
+// function isLoggedInOrSubscribed(loggedIn, subscribed) {
+// return loggedIn === "LOGGED_IN" || subscribed === "SUBSCRIBED";
+// }
+
+// console.log(isLoggedInOrSubscribed("LOGGED_IN", "SUBSCRIBED"));
+
+
+
+
+
+
---
+++ JavaScript - Challenges/Medium Challenges/app.js
@@ -0,0 +1,26 @@
+// Falsy or Truthy? given 2 values. if one is true return false. likewise.
+
+
+// function falsyOrTruthy(elem1, elem2) {
+//     return !elem1 ? elem1 : elem2
+
+// }
+
+// console.log(falsyOrTruthy(0, 5));
+
+// Return the length of any given array
+
+
+// function arrLength(arr) {
+//     return arr.length;
+// }
+// console.log(arrLength([1, 2, 3, 4, 5, 6]));
+
+// Get the last element in an array
+
+
+function arrLength(arr) {
+    return arr[arr.length - 1];
+}
+
+console.log(arrLength([1, 2, 3, 4, 5, 6]));
