35f5b32a1d1add0c88643e57efcd9bf9ea27cdf4
ChrisDi75 <104084510+ChrisDi75@users.noreply.github.com>
2022-07-16 23:57:19 +0200

    advanced challenges

---
+++ JavaScript - Challenges/Advanced Challenges/app.js
@@ -0,0 +1,42 @@
+// Show Rating
+    //given a rating, display a star * for each full rating and a full-stop . for each half rating 
+
+    // function showRating(rating) {
+    //     let ratings= "";
+    //     for(let i = 0; i < Math.floor(rating); ++i){
+    //         ratings = ratings + "*"
+    //         if (i !== Math.floor(rating) - 1){
+    //             ratings = ratings + " ";
+    //         }
+    //     }
+    //     if(!Number.isInteger(rating)){
+    //         ratings = ratings + " .";
+    //     }
+    //     return ratings;
+    // }
+
+    // console.log(showRating(.5));
+
+
+
+    // Sort by lowest to highest price 
+
+    // function sortLowToHigh(numbers){
+    //     return numbers.sort((a, b) => a - b);
+
+    // }
+
+    // console.log(sortLowToHigh([1, 5, 0, 10, 4]));
+
+    //  Sort by highest to lowest Price
+
+    // function sortHightoLow (numbers) {
+    //     return numbers.sort((a, b) => b.price - a.price);
+
+    // }
+
+    // console.log(sortHightoLow([{id: 1, price: 50}, {id: 2, price: 30}, {id: 3, price: 60}, {id: 4, price: 10}]));
+
+
+    
+    // Promises
